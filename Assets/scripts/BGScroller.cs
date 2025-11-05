1b55544ed4a70b5c7d3ab368fdc7e83278a9e090
Tameeah <tameeah.padayachy@gmail.com>
2025-11-05 16:20:11 +0200

    UI Updated
    updated Controls Panel, gun panel and Win and lose scene BG

---
+++ Assets/scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    [Range(-1f, 1f)]
+
+    public float scrollSpeed = 0.5f;
+    private float offSet;
+    private Material mat;
+
+    private void Start()
+    {
+       mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        offSet += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offSet, 0));
+    }
+
+}
